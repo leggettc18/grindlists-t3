e2296b72253b9a660eb991efb474916b80462f0a
Christopher Leggett <chris@leggett.dev>
2023-07-11 12:50:22 -0400

    Removes example router.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { listRouter } from "~/server/api/routers/lists";
 import { listItemRouter } from "~/server/api/routers/listItem";
@@ -10,7 +9,6 @@ import { itemRouter } from "~/server/api/routers/items";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-    example: exampleRouter,
     list: listRouter,
     listItem: listItemRouter,
     item: itemRouter,
